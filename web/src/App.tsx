3800209d6db90e74eae62011b3570616e2eee1da
Damir Nureev <damirn29@gmail.com>
2024-03-07 15:23:14 +0300

    fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,19 @@
-import MainRouter from './mainRouter';
+import { useState } from 'react';
+import MainRouter from './MainRouter';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
+
+  const handleAuthentication = () => {
+    setIsAuth(!isAuth);
+  };
+
+
+
   return (
     <div>
-      <MainRouter isAuth={false} />
+      <MainRouter isAuth={isAuth} handleAuthentication={handleAuthentication} />
     </div>
   );
 }
---
+++ web/src/navbar/colors.ts
@@ -0,0 +1,6 @@
+export const lightTextColor = '#000000';
+export const darkTextColor = '#dddddd';
+export const lightBackgroundColor = '#ffffff';
+export const darkBackgroundColor = '#333333';
+export const lightButtonColor = '#878787';
+export const darkButtonColor = '#aaaaaa';
