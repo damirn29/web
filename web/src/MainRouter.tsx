faf6a3a06dbccf210a853de8ccee079dbe40aae8
Damir Nureev <damirn29@gmail.com>
2024-03-17 21:41:46 +0300

    add form

--- web/src/MainRouter.tsx
+++ web/src/MainRouter.tsx
@@ -4,13 +4,15 @@ import Home from './pages/home/home';
 import Tasks from './pages/tasks/Tasks';
 import About from './pages/about/About';
 import Contact from './pages/contact/contact';
-import Navbar from './navbar/Navbar';
+import Login from './navbar/login';
+import StyledNav from './navbar/Navbar';
 
 const MainRouter: React.FC<{ isAuth: boolean, handleAuthentication: () => void }> = ({ isAuth, handleAuthentication }) => {
   return (
     <div>
-      <Navbar isAuth={isAuth} handleAuthentication={handleAuthentication} />
+      <StyledNav isAuth={isAuth} handleAuthentication={handleAuthentication} />
       <Routes>
+        <Route path="/login" element={<Login handleAuthentication={handleAuthentication} />} />
         <Route path="/" element={<Home />} />
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/about" element={<About />} />
