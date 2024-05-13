74c77e03bd05d952ecd309feff2f73d96bdb0971
Damir Nureev <damirn29@gmail.com>
2024-05-13 23:26:17 +0300

    add tests

---
+++ web/tests/Form.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Contact from '../src/pages/contact/contact'
+
+describe('Contact', () => {
+    it('renders contact information', () => {
+        render(<Contact />);
+
+        // Проверка заголовка
+        const heading = screen.getByText('Контакты');
+        if (!heading) throw new Error('Heading not found');
+
+        // Проверка наличия текста с информацией о контактах
+        const description = screen.getByText('Вы можете связаться с нами, используя следующие контактные данные.');
+        if (!description) throw new Error('Description not found');
+
+        const email = screen.getByText('Email: example@example.com');
+        if (!email) throw new Error('Email information not found');
+
+        const phone = screen.getByText('Телефон: +123456789');
+        if (!phone) throw new Error('Phone information not found');
+    });
+});
